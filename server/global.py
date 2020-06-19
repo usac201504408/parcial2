74282e91f411563d9cfd58b35482dff52632ac2a
usac201504408 <3001650050101@ingenieria.usac.edu.gt>
2020-06-19 14:44:56 -0700

    creacion de carpetas server y un cliente, archivos de configuracion global

---
+++ server/global.py
@@ -0,0 +1,13 @@
+ALIVE_PERIOD = 2 #JPGM Período entre envío de tramas ALIVE
+ALIVE_CONTINUOUS = 0.1 #JPGM Período entre envío de tramas ALIVE si no hay respuesta
+
+#JPGM COMMANDS
+COMMAND_FTR = b'\x03'
+COMMAND_ALIVE = b'\x04'
+COMMAND_ACK = b'\x05'
+COMMAND_OK = b'\x06'
+COMMAND_NO = b'\x07'
+
+#JPGM System filenames
+USERS_FILENAME = 'usuarios'
+ROOMS_FILENAME = 'salas'
