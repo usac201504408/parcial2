649b445fa99af2feac90bfddeb06241e4d5fd187
usac201504408 <3001650050101@ingenieria.usac.edu.gt>
2020-06-20 06:52:12 -0700

    cliente suscribiendose a topics, lectura de archivo de texto topics

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,1 +1,81 @@
 #JPGM clase para manejo de cliente
+#imports
+import paho.mqtt.client as mqtt
+from broker import *
+import logging
+import time
+import lecturaArchivos
+
+
+#Configuracion inicial de logging
+logging.basicConfig(
+    level = logging.INFO, 
+    format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+    )
+
+#Handler en caso suceda la conexion con el broker MQTT
+def on_connect(client, userdata, flags, rc): 
+    connectionText = "CONNACK recibido del broker con codigo: " + str(rc)
+    logging.info(connectionText)
+    #AQUI COLOCAR HILO ALIVE
+
+#Handler en caso se publique satisfactoriamente en el broker MQTT
+def on_publish(client, userdata, mid): 
+    publishText = "Publicacion satisfactoria"
+    logging.info(publishText)
+    
+
+
+#Callback que se ejecuta cuando llega un mensaje al topic suscrito
+def on_message(client, userdata, msg):
+    #Se muestra en pantalla informacion que ha llegado
+    logging.info("Ha llegado el mensaje al topic: " + str(msg.topic))
+    mensajedecode =  msg.payload.decode()
+  
+    logging.info("El contenido del mensaje es: " + str(mensajedecode))
+    
+   
+
+#conexion a mqtt
+#configurar al cliente como publisher y subscriber
+client = mqtt.Client(clean_session=True) #Nueva instancia de cliente
+client.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
+client.on_message = on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
+client.on_publish = on_publish #Se configura la funcion "Handler" que se activa al publicar algo
+client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
+client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
+
+
+#Nos conectaremos a distintos topics:
+qos = 2
+#Subscripcion simple con tupla (topic,qos)
+# client.subscribe(("comandos/14/201504408", qos))
+#suscribirse a todos los topics del archivo
+topics = lecturaArchivos.LecturaArchivo("topics.txt").getArreglo()
+for topic in topics:
+    client.subscribe((str(topic), qos))
+
+
+
+#Mensaje de prueba MQTT en el topic "test"
+client.publish("comandos/14/201504408", "Mensaje inicial", qos = 2, retain = False)
+#Mensaje de prueba MQTT en el topic "test"
+client.publish("comandos/14/201504408", "Mensaje inicial2", qos = 2, retain = False)
+#Iniciamos el thread (implementado en paho-mqtt) para estar atentos a mensajes en los topics subscritos
+client.loop_start()
+
+#El thread de MQTT queda en el fondo, mientras en el main loop hacemos otra cosa
+try:
+    while True:
+        logging.info("olakease")
+        time.sleep(10)
+        
+
+
+except KeyboardInterrupt:
+    logging.warning("Desconectando del broker...")
+
+finally:
+    client.loop_stop() #Se mata el hilo que verifica los topics en el fondo
+    client.disconnect() #Se desconecta del broker
+    logging.info("Desconectado del broker. Saliendo...")
---
+++ cliente/lecturaArchivos.py
@@ -0,0 +1,22 @@
+#clase para lectura de archivos
+
+
+class LecturaArchivo(object):
+
+    def __init__(self, nombreArchivo):
+        self.nombreArchivo = nombreArchivo
+
+    def getArreglo(self):
+        datos = list()
+        archivo = open(self.nombreArchivo, 'r')
+        for linea in archivo:
+            datos.append(linea.replace('\n', '').strip())
+        archivo.close()
+        return datos
+
+
+#Ejemplo de consumo de la clase
+# nuevoarreglo = LecturaArchivo("topics.txt").getArreglo()
+# print(nuevoarreglo)
+     
+
